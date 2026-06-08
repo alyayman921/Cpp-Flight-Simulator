2a30d6532a2470b9d37829c34c8a948b06010a99
Aly Sarhan <alyayman921@gmail.com>
2026-06-08 04:43:32 +0300

    Importing xlsx working

--- Autopilot.c
+++ Autopilot.c
@@ -2,20 +2,11 @@
 #include "derivatives.h"
 int main(){
 
-const char filename[]="C5A.xlsx"; // filename itself this is a const char pointer to c5a string 
+const char filename[]="meta/C5A.xlsx"; // filename itself this is a const char pointer to c5a string 
 
 raw_data raw=readxlsx(filename); 
 aircraft_data acd=sorting(raw);
-int i=0;
-while ( i < 62){
-//printf("B%d is %f\n",i,raw.B[i]);
-	i++;
-}
-i=0;
-while ( i < 3){
-printf("V%d is %f\n",i,acd.V[i]);
-	i++;
-}
+
 while (false){
 
 }
